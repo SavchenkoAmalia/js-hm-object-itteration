a566b8501e48fac4a8dc7cd6a734727b437b0c51
SavchenkoAmalia <ameli.savchenko@gmail.com>
2024-10-03 18:07:28 +0300

    add all

--- js/script.js
+++ js/script.js
@@ -1,99 +1,99 @@
 //! 1
 
-// const user ={
-//     name: 'Bob',
-//     age: 30,
-//     hobby: 'play voleyball',
-//     premium: true
-// }
+const user ={
+    name: 'Bob',
+    age: 30,
+    hobby: 'play voleyball',
+    premium: true
+}
 
-// user.mood = 'happy';
-// user.hobby = 'skydiving';
-// user.premium = false;
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
 
-// for (let key of Object.keys(user)) {
-//     console.log(`${key}: ${user[key]}`);
-// }
+for (let key of Object.keys(user)) {
+    console.log(`${key}: ${user[key]}`);
+}
 
 //! 2
 
-// const user1 ={
-//     name: 'Bob',
-//     age: 30,
-//     hobby: 'play voleyball',
-//     premium: true,
-// }
+const user1 ={
+    name: 'Bob',
+    age: 30,
+    hobby: 'play voleyball',
+    premium: true,
+}
 
-// function countProps(obj) {
-//     return Object.keys(obj).length;
-// }
+function countProps(obj) {
+    return Object.keys(obj).length;
+}
 
-// console.log(countProps(user1))
+console.log(countProps(user1))
 
 //! 3
 
-// const employees = {
-//     Maksim: 23,
-//     oleg: 14,
-//     Bob: 28,
-//     Pavlo: 18,
-//     Oleksandr: 26
-// }
+const employees = {
+    Maksim: 23,
+    oleg: 14,
+    Bob: 28,
+    Pavlo: 18,
+    Oleksandr: 26
+}
 
-// function findBestEmployee(employees){
-//     let maxTasks = 0;
-//     let bestEmployee = '';
+function findBestEmployee(employees){
+    let maxTasks = 0;
+    let bestEmployee = '';
 
-//     for (const [name, tasks] of Object.entries(employees)) {
-//         if (tasks > maxTasks) {
-//             maxTasks = tasks;
-//             bestEmployee = name;
-//         }
-//     }
+    for (const [name, tasks] of Object.entries(employees)) {
+        if (tasks > maxTasks) {
+            maxTasks = tasks;
+            bestEmployee = name;
+        }
+    }
 
-//     return `${bestEmployee}: ${maxTasks}`;
-// }
-// console.log(findBestEmployee(employees));
+    return `${bestEmployee}: ${maxTasks}`;
+}
+console.log(findBestEmployee(employees));
 
 //! 4
 
-// const employees = {
-//     Maksim: 23000,
-//     oleg: 14000,
-//     Bob: 28000,
-//     Pavlo: 18000,
-//     Oleksandr: 26000
-// }
+const employees = {
+    Maksim: 23000,
+    oleg: 14000,
+    Bob: 28000,
+    Pavlo: 18000,
+    Oleksandr: 26000
+}
 
-// function countTotalSalary(employees){
-//     let allSume = 0;
-//     for(let value in employees){
-//         allSume += employees[value];
-//     }
-//     return allSume;
-// }
+function countTotalSalary(employees){
+    let allSume = 0;
+    for(let value in employees){
+        allSume += employees[value];
+    }
+    return allSume;
+}
 
-// console.log(`загальна сума всіх зарплат: ${countTotalSalary(employees)}`)
+console.log(`загальна сума всіх зарплат: ${countTotalSalary(employees)}`)
 
 //! 5
 
-// let arr = [
-//     { name: 'Amalia', age: 14},
-//     { name: 'Denis', age: 12},
-//     { name: 'Sacha', age: 16},
-//     { name: 'Maksim', age: 10}
-// ]
+let arr = [
+    { name: 'Amalia', age: 14},
+    { name: 'Denis', age: 12},
+    { name: 'Sacha', age: 16},
+    { name: 'Maksim', age: 10}
+]
 
-// function getAllPropValues(arr, prop) {
-//     const values = [];
-//     for(let obj of arr){
-//         values.push(obj[prop]);
-//     }
-//     return values;
+function getAllPropValues(arr, prop) {
+    const values = [];
+    for(let obj of arr){
+        values.push(obj[prop]);
+    }
+    return values;
 
-// }
+}
 
-// console.log(getAllPropValues(arr, 'age'));
+console.log(getAllPropValues(arr, 'age'));
 
 //! 6
 
